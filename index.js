b707f6035e2834d7defc043de4cb9989d7e0f16e
Guilherme Rodrigues <guilhermecarvalhorodrigues@gmail.com>
2024-08-14 21:25:36 -0300

    Finalizado Aula 02

---
+++ index.js
@@ -0,0 +1,59 @@
+/*const nome = "Guilherme"
+
+const sobrenome = "Rodrigues"
+
+const nascimento = 29
+
+const atual = 2024
+
+const completo = nome + " " + sobrenome
+
+const idade = atual - nascimento
+
+console.log(completo)
+
+console.log(idade)*/
+
+//Prompt
+//Console
+//Alert
+
+const numero1 = Number(prompt("Digite um número"))
+const numero2 = Number(prompt("Digite outro número"))
+alert(numero1 + numero2)
+
+
+
+
+
+
+
+
+
+
+
+/*pessoa.idade = 30
+
+console.log(pessoa)
+
+const numero1 = 3
+
+const numero2 = 6
+
+const soma = numero1 + numero2
+
+const sub = numero1 - numero2
+
+const mult = numero1 * numero2
+
+const div = numero1 / numero2
+
+const exp = numero1 ** numero2
+
+const rest = numero1 % numnero2
+
+
+
+//comentário de linha
+
+/* comentário em bloco */
